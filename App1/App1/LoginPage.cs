24c71529d08b9576425d441fe310d52335e67aa1
RommelAlonzo-exigo <rommela@exigo.com>
2016-09-07 09:40:28 -0500

    Add project files.

---
+++ App1/App1/LoginPage.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public class UsernameViewModel : INotifyPropertyChanged
+    {
+        private INavigation navigation;
+        public UsernameViewModel(INavigation navigation)
+        {
+            this.navigation = navigation;
+        }
+
+
+        private string username = string.Empty;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                if (username == value)
+                    return;
+
+                username = value;
+                OnPropertyChanged("Username");
+                OnPropertyChanged("Display");
+            }
+        }
+
+        public string Display
+        {
+            get { return "Hello there: " + username; }
+        }
+
+        private Command loginCommand;
+        public Command LoginCommand
+        {
+            get { return loginCommand ?? 
+                    (loginCommand = new Command<string>(ExecuteLoginCommand, CanLogin));
+            }
+        }
+
+        private void ExecuteLoginCommand(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return;
+
+            navigation.PushAsync(new TimeListPage());
+        }
+
+        private bool CanLogin(string name)
+        {
+            return !string.IsNullOrWhiteSpace(name);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged == null)
+                return;
+
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            this.BindingContext = new UsernameViewModel(Navigation);
+            var entry = new Entry
+            {
+                Placeholder = "Enter Username"
+            };
+            entry.SetBinding<UsernameViewModel>(Entry.TextProperty,
+                vm => vm.Username, BindingMode.TwoWay);
+
+            var label = new Label
+            {
+
+            };
+
+            label.SetBinding<UsernameViewModel>(Label.TextProperty,
+                vm => vm.Display);
+
+            var button = new Button
+            {
+                Text = "Login"
+            };
+
+            button.SetBinding<UsernameViewModel>(Button.CommandProperty,
+                vm => vm.LoginCommand);
+
+            button.SetBinding<UsernameViewModel>(Button.CommandParameterProperty,
+                vm => vm.Username);
+
+            Content = new StackLayout
+            {
+                Padding = 10,
+                Spacing = 10,
+                Children = { entry, label, button }
+            };
+        }
+    }
+}
---
+++ App1/App1/TimeListPage.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public class ListPage: ContentPage
+    {
+        ObservableCollection<string> Items;
+
+        public ListPage()
+        {
+            this.Title = "List";
+            Items = new ObservableCollection<string>();
+             
+            var list = new ListView();
+            list.ItemsSource = Items;
+
+            list.ItemSelected += (sender, args) =>
+            {
+                if (list.SelectedItem == null)
+                    return;
+
+                this.Navigation.PushAsync(new ContentPage
+                {
+                    Title = "Page 2",
+                    Content = new Label
+                    {
+                        Text = args.SelectedItem as string
+                    }
+                });
+                list.SelectedItem = null;
+            };
+
+            var button = new Button
+            {
+                Text = "Add Time"
+            };
+
+            button.Clicked += (sender, args) =>
+            {
+                Items.Add(DateTime.Now.ToString("F"));
+            };
+
+            var stack = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Spacing = 10,
+                Children = { list, button }
+            };
+
+            Content = stack;
+        }
+    }
+
+    public class TimeListPage : TabbedPage
+    {
+        public TimeListPage()
+        {
+            this.Title = "Time List";
+            Children.Add(new ListPage());
+
+            Children.Add(new ContentPage
+            {
+                Title = "tab2",
+                Padding = 10,
+                Content = new Label
+                {
+                    Text = "Hello from Tab2"
+                }
+            });
+        }
+    }
+}
